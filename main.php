ee2f275d27262efcadf0b1f0a14348d54a68a8c9
angegevela <202280079@psu.palawan.edu.ph>
2024-11-27 10:19:32 +0800

    Inputting the html structure which is the text and button.

--- main.php
+++ main.php
@@ -6,6 +6,12 @@
     <title>Math Quiz Welcome Page</title>
 </head>
 <body>
-    
+<div id="wrapper">
+<h3>Welcome to the Math Quiz Calculator</h3>
+<p>Get ready to challenge yourself with math problems and sharpen your skills!</p>
+		<form action="index1.php" method="GET">
+			<input type="submit" value="Let's Get Started" name="submit" id="submit" />
+		</>
+</div>
 </body>
 </html>
