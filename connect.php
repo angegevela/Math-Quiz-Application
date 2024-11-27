fbf2b16fd46ab5c91569b434b73597c744ac8c54
angegevela <202280079@psu.palawan.edu.ph>
2024-11-27 10:15:59 +0800

    Conditional statements if the php and sql cannot connect to each other and let's say that the storing is not possible, it this part I also added a database to store datasin my math quiz application.

--- connect.php
+++ connect.php
@@ -5,6 +5,13 @@ $password = "mysqlroot";
 $dbname = "math_quiz"; //In this part I created a new database for my leaderboard
 
 
+//db overall information given above.
+$conn = new mysqli($servername, $username, $password, $dbname);
 
+//condition statement where if the xammpp does not connect in the sql or an error it will go error, meanwhile if it is good it, directs to echo.
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
 
 ?>
