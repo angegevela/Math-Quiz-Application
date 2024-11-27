cae8038d436572a89d9bbf8870a07ea1ce175691
angegevela <202280079@psu.palawan.edu.ph>
2024-11-27 11:35:12 +0800

    Check if the user has completed the quiz.

--- results.php
+++ results.php
@@ -3,7 +3,11 @@ session_start();
 include('connect.php');
 
 
-
+// Check if the user has completed the quiz
+if (!isset($_SESSION['score'])) {
+    header('Location: quiz.php');
+    exit;
+}
 
 
 
