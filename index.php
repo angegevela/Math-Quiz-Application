81eec04386e781c36dc50e58a07e48d48e5aaa24
angegevela <202280079@psu.palawan.edu.ph>
2024-11-27 10:31:43 +0800

    adding the button and form code, this will link to other filewhere the user choose, it include the close button where it will terminate the process of the game and start quiz.

--- index.php
+++ index.php
@@ -4,3 +4,24 @@ include('connect.php');
 $sql_query = "SELECT username, score, date FROM leaderboard ORDER BY score DESC, date ASC LIMIT 10";
 $result = $conn->query($sql_query);
 ?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LeaderBoard</title>
+</head>
+<body>
+<h1>Leaderboard</h1>
+    <form method="POST" action="settings.php">
+    <button type="submit" name="start_quiz">Start Quiz</button>
+    </form>
+
+    <form method="POST">
+        <button type="submit" name="close">Close</button>
+    </form>
+
+</body>
+</html>
